0baa555846abb540c951361309a8b9bcef92c917
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-04-09 09:47:26 +0200

    chore: Squash

--- .deployment/entrypoint.sh
+++ .deployment/entrypoint.sh
@@ -14,11 +14,6 @@ if [ ! "$(sha1sum -c package-lock.sha1)" ]; then
   echo "⚠️ Need to install packages due to updated package-lock.json"
   # When the checksums are not matching perform an npm install
   npm ci --ignore-scripts
-
-  # TODO: saving of the builders can be removed after #844 is merged
-  # THen the builders arn't located in the node modules anymore
-  cp -R ./dist/tmp/workspace ./node_modules/@dynatrace/workspace
-  cp -R ./dist/tmp/barista-builders ./node_modules/@dynatrace/barista-builders
 fi
 
 # Run the command from the docker image
